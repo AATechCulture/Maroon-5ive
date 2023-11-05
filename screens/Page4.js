4fdc8604b68deab82055f76533fd3d0e557a2898
Asa <cyberasasoftware@gmail.com>
2023-11-05 02:29:17 -0600

    done with page 5

--- screens/Page4.js
+++ screens/Page4.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   View,
   Text,
@@ -6,11 +6,11 @@ import {
   Dimensions,
   ScrollView,
   TouchableOpacity,
-} from 'react-native';
+} from "react-native";
 
 const totalSeats = 172; // Total number of seats
 const seatsPerRow = 3; // Number of seats per row
-const screenWidth = Dimensions.get('window').width; // Get the screen width
+const screenWidth = Dimensions.get("window").width; // Get the screen width
 const seatSize = screenWidth / (seatsPerRow * 2.5); // Calculate seat size based on screen width
 
 const numRows = Math.ceil(totalSeats / (seatsPerRow * 2));
@@ -39,69 +39,87 @@ for (let row = 0; row < numRows; row++) {
 }
 
 const styles = StyleSheet.create({
+  buttonContainer:{
+    width:"100%",
+    display: "flex",
+    alignItems: 'center',     // Center horizontally
+  },
+  button: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
   container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     paddingHorizontal: 10,
-    backgroundColor: 'black',
+    backgroundColor: "black",
     paddingTop: 20,
   },
   side: {
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-end', // Align to the right
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-end", // Align to the right
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   seat: {
     width: seatSize,
     height: seatSize,
     margin: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 10, // Adjust the borderRadius for soft edges
   },
   seatOccupied: {
-    backgroundColor: 'grey',
+    backgroundColor: "grey",
   },
   seatAvailable: {
-    backgroundColor: 'darkblue',
+    backgroundColor: "darkblue",
   },
   seatSelected: {
-    backgroundColor: 'grey',
-    borderWidth: 2,      // Add a border width
-    borderColor: 'white', // Set the border color to white
+    backgroundColor: "grey",
+    borderWidth: 2, // Add a border width
+    borderColor: "white", // Set the border color to white
   },
   seatText: {
-    color: 'white',
+    color: "white",
   },
   rowNumberContainer: {
     width: 20,
-    alignItems: 'flex-end', // Align row numbers to the right
+    alignItems: "flex-end", // Align row numbers to the right
   },
   rowNumberText: {
     fontSize: 14,
-    color: 'white',
+    color: "white",
   },
 });
-
+var navigation;
 export default class Page4 extends Component {
   constructor(props) {
     super(props);
+    navigation = this.props.route.params.navigation
     this.state = {
       selectedLeftSeats: [], // Track the currently selected seats in the left column
       selectedRightSeats: [], // Track the currently selected seats in the right column
       storedNumber: props.groupnumber, // Store the parsed number here
-      setgroupnumber: props.setgroupnumber
+      setgroupnumber: props.setgroupnumber,
     };
   }
 
   handleLeftSeatPress(row, col) {
     const { selectedLeftSeats } = this.state;
-    const isSelected = selectedLeftSeats.some((seat) => seat.row === row && seat.col === col);
+    const isSelected = selectedLeftSeats.some(
+      (seat) => seat.row === row && seat.col === col
+    );
 
     if (isSelected) {
       // Deselect the seat
@@ -109,18 +127,20 @@ export default class Page4 extends Component {
         (seat) => !(seat.row === row && seat.col === col)
       );
       this.setState({ selectedLeftSeats: updatedSelectedSeats });
-    } else if (selectedLeftSeats.length <= 5) { // Check if less than 5 seats are selected
+    } else if (selectedLeftSeats.length <= 5) {
+      // Check if less than 5 seats are selected
       // Select the seat
       this.setState({
         selectedLeftSeats: [...selectedLeftSeats, { row, col }],
       });
-      
     }
   }
 
   handleRightSeatPress(row, col) {
     const { selectedRightSeats } = this.state;
-    const isSelected = selectedRightSeats.some((seat) => seat.row === row &&  seat.col === col);
+    const isSelected = selectedRightSeats.some(
+      (seat) => seat.row === row && seat.col === col
+    );
 
     if (isSelected) {
       // Deselect the seat
@@ -128,7 +148,8 @@ export default class Page4 extends Component {
         (seat) => !(seat.row === row && seat.col === col)
       );
       this.setState({ selectedRightSeats: updatedSelectedSeats });
-    } else if (selectedRightSeats.length < 5) { // Check if less than 5 seats are selected
+    } else if (selectedRightSeats.length < 5) {
+      // Check if less than 5 seats are selected
       // Select the seat
       this.setState({
         selectedRightSeats: [...selectedRightSeats, { row, col }],
@@ -138,75 +159,93 @@ export default class Page4 extends Component {
 
   isLeftSeatSelected(row, col) {
     const { selectedLeftSeats } = this.state;
-    return selectedLeftSeats.some((seat) => seat.row === row && seat.col === col);
+    return selectedLeftSeats.some(
+      (seat) => seat.row === row && seat.col === col
+    );
   }
 
   isRightSeatSelected(row, col) {
     const { selectedRightSeats } = this.state;
-    return selectedRightSeats.some((seat) => seat.row === row && seat.col === col);
+    return selectedRightSeats.some(
+      (seat) => seat.row === row && seat.col === col
+    );
   }
 
   render() {
     return (
-      <ScrollView>
-        <View style={styles.container}>
-          <View style={styles.side}>
-            {leftSideSeats.map((row, rowIndex) => (
-              <View key={rowIndex} style={styles.row}>
-                {row.map((isOccupied, colIndex) => (
-                  <TouchableOpacity
-                    key={colIndex}
-                    style={[
-                      styles.seat,
-                      isOccupied
-                        ? styles.seatOccupied
-                        : this.isLeftSeatSelected(rowIndex, colIndex)
-                        ? styles.seatSelected
-                        : styles.seatAvailable,
-                    ]}
-                    onPress={() => this.handleLeftSeatPress(rowIndex, colIndex)}
-                  >
-                    <View>
-                      <Text style={styles.seatText}>
-                        {isOccupied ? 'X' : '0'}
-                      </Text>
-                    </View>
-                  </TouchableOpacity>
-                ))}
-                <View style={styles.rowNumberContainer}>
-                  <Text style={styles.rowNumberText}>{rowIndex + 1}</Text>
-                </View>
-              </View>
-            ))}
-          </View>
-          <View style={styles.side}>
-            {rightSideSeats.map((row, rowIndex) => (
-              <View key={rowIndex} style={styles.row}>
-                {row.map((isOccupied, colIndex) => (
-                  <TouchableOpacity
-                    key={colIndex}
-                    style={[
-                      styles.seat,
-                      isOccupied
-                        ? styles.seatOccupied
-                        : this.isRightSeatSelected(rowIndex, colIndex)
-                        ? styles.seatSelected
-                        : styles.seatAvailable,
-                    ]}
-                    onPress={() => this.handleRightSeatPress(rowIndex, colIndex)}
-                  >
-                    <View>
-                      <Text style={styles.seatText}>
-                        {isOccupied ? 'X' : '0'}
-                      </Text>
-                    </View>
-                  </TouchableOpacity>
-                ))}
-              </View>
-            ))}
-          </View>
+      <View>
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}
+
+            onPress={() => navigation.navigate("Page5")}
+          >
+            <Text style={styles.buttonText}>Next</Text>
+      </TouchableOpacity>
         </View>
-      </ScrollView>
+        <ScrollView>
+          <View style={styles.container}>
+            <View style={styles.side}>
+              {leftSideSeats.map((row, rowIndex) => (
+                <View key={rowIndex} style={styles.row}>
+                  {row.map((isOccupied, colIndex) => (
+                    <TouchableOpacity
+                      key={colIndex}
+                      style={[
+                        styles.seat,
+                        isOccupied
+                          ? styles.seatOccupied
+                          : this.isLeftSeatSelected(rowIndex, colIndex)
+                          ? styles.seatSelected
+                          : styles.seatAvailable,
+                      ]}
+                      onPress={() =>
+                        this.handleLeftSeatPress(rowIndex, colIndex)
+                      }
+                    >
+                      <View>
+                        <Text style={styles.seatText}>
+                          {isOccupied ? "X" : "0"}
+                        </Text>
+                      </View>
+                    </TouchableOpacity>
+                  ))}
+                  <View style={styles.rowNumberContainer}>
+                    <Text style={styles.rowNumberText}>{rowIndex + 1}</Text>
+                  </View>
+                </View>
+              ))}
+            </View>
+            <View style={styles.side}>
+              {rightSideSeats.map((row, rowIndex) => (
+                <View key={rowIndex} style={styles.row}>
+                  {row.map((isOccupied, colIndex) => (
+                    <TouchableOpacity
+                      key={colIndex}
+                      style={[
+                        styles.seat,
+                        isOccupied
+                          ? styles.seatOccupied
+                          : this.isRightSeatSelected(rowIndex, colIndex)
+                          ? styles.seatSelected
+                          : styles.seatAvailable,
+                      ]}
+                      onPress={() =>
+                        this.handleRightSeatPress(rowIndex, colIndex)
+                      }
+                    >
+                      <View>
+                        <Text style={styles.seatText}>
+                          {isOccupied ? "X" : "0"}
+                        </Text>
+                      </View>
+                    </TouchableOpacity>
+                  ))}
+                </View>
+              ))}
+            </View>
+          </View>
+        </ScrollView>
+      </View>
     );
   }
 }
