2264049b664f0fd49f3ecae42814babec4107891
taywalker <106616846+taywalker@users.noreply.github.com>
2023-11-05 09:01:03 -0600

    Component box

--- screens/Page3.js
+++ screens/Page3.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -11,10 +11,53 @@ import {
 } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import styles from "../components/Styles";
+import FlightApi from "../api/FlightData";
+
+function DynamicComponent() {
+  return (
+    <View style={styles.dynamicComponent}>
+      <Text>Dynamic Component</Text>
+    </View>
+  );
+}
 
 
 export default function Page3({ navigation }) {
   const [seatsToOccupy, setSeatsToOccupy] = useState(2);
+  const [flightData, setFlightData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        // const data = await FlightApi();
+        setFlightData(data);
+        console.log(data);
+        console.log("got data");
+      } catch (error) {
+        console.error("Error fetching flight data:", error);
+      }
+    }
+
+    // fetchData();
+  }, []); // The empty dependency array ensures this effect runs only once, like componentDidMount
+
+  const componentsToRender = [];
+
+  for (let i = 0; i < 5; i++) {
+    componentsToRender.push(<DynamicComponent key={i} />);
+  }
+
+  // return (
+  //   <View style={styles.startBody}>
+  //     <View style={styles.questionContainer}>
+  //       <Text>dynamic rendering below</Text>
+  //       <View>{componentsToRender}</View>
+  //     </View>
+  //     <View style={styles.exclusiveText}>
+  //       <TouchableOpacity
+  //         onPress={() =>
+  //           navigation.navigate("Page4", { seatsToOccupy, navigation, flightData })
+  //         }
 
   return (
     <View style={styles.startBody}>
@@ -32,7 +75,7 @@ export default function Page3({ navigation }) {
         </Text>  
       </View>
       <TouchableOpacity
-          onPress={() => navigation.navigate("Page4", { seatsToOccupy })}
+          onPress={() => navigation.navigate("Page4", { navigation })}
         >
           <Text>Page 4</Text>
         </TouchableOpacity>
