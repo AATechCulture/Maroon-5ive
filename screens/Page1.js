b2f8143d432a0e9151316fac5da2150fdee52b88
taywalker <106616846+taywalker@users.noreply.github.com>
2023-11-04 22:05:45 -0500

    Page1 done

--- screens/Page1.js
+++ screens/Page1.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StatusBar, Button, ImageBackground } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StatusBar, Button, ImageBackground, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import styles from '../components/Styles';
 import Footer from '../Footer';
 import { StyleSheet } from 'react-native';
 import Sky from "../Images/sky.png"
+import { image } from 'react-native';
 
-export default function Page1({ navigation}) {
+export default function Page1({ navigation }) {
 
 
 
@@ -21,32 +22,72 @@ export default function Page1({ navigation}) {
   };
 
   return (
-    
     <View style={styles.startBody}>
-      <ImageBackground 
-          source={require("../Images/sky.png")}
-          style={styles.background}
-        />
-      <View style={styles.exclusiveText}>
-      </View>
+      <ImageBackground
+        source={require("../Images/sky.png")}
+        style={background.backgroundImage}
+      >
+        <View style={style.container}>
+          <Image
+            source={require("C:/Users/ttaay/repo/Maroon-5ive/Images/MikeJones.png")}
+            style={style.image}
+            resizeMode='center'
+          />
+        </View>
 
-      <TouchableOpacity onPress={() => navigation.navigate('Page2')}>
+        <View style={style.container}>
+          <Image
+            source={require("C:/Users/ttaay/repo/Maroon-5ive/Images/TheJonesFamily.png")}
+            style={style.image2}
+            resizeMode='center'
+          />
+        </View>
+
+        <View style={style.container}>
+          <Image
+            source={require("C:/Users/ttaay/repo/Maroon-5ive/Images/Miles.png")}
+            style={style.image3}
+            resizeMode='center'
+          />
+        </View>
+
+        <TouchableOpacity onPress={() => navigation.navigate('Page2')}>
           <Footer />
-      </TouchableOpacity>
-    
-    
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
-    
   );
 };
 const background = StyleSheet.create({
   backgroundImage: {
     flex: 1,
     width: '100%',
-    height: '50%',
-    resizeMode: 'contain', 
-    backgroundColor: "rgba(0,0,0,0)"
-  },
+    height: '100%',
+    resizeMode: 'contain',
+  }
+});
 
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  image: {
+    width: 400,
+    height: 220,
+    bottom: 0,
+  },
+  image2: {
+  width: 400,
+  height: 220,
+  bottom: 100,
+  },
+  image3: {
+    width: 400,
+    height: 220,
+    bottom: 100,
+},
 });
 
