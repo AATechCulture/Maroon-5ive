a44491ca73b0f257325ca5cfd5647e91e4a7e9cf
Eyerusalem-hub <79621412+Eyerusalem-hub@users.noreply.github.com>
2023-11-05 01:50:15 -0600

    background/checkbox

--- components/Styles.js
+++ components/Styles.js
@@ -1,128 +1,186 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    button: {
-      backgroundColor: 'blue',
-      padding: 10,
-      borderRadius: 5,
-      width: '80%',
-      height: '40px',
-      backgroundColor: '#2980b9',
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: '5px',
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 16,
-      fontWeight: 'bold',
-    },
-    Input: {
-        width: '80%',
-        height: '40px',
-        backgroundColor: 'rgba(255, 255, 255, 0.7)',
-        marginBottom: '20px',
-        padding: '10px',
-        borderRadius: '5px',
-    },
-    startBody: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      exclusiveText: {
-        height: "20%",
-        width: '15%',
-        marginRight: '15%',
-        fontWeight: '1000',
-        fontSize: 4,
-        color: 'white',
-      },
-      startContainer: {
-        display: 'flex',
-        justifyContent: 'center',
-      },
-      startSection: {
-        position: 'relative',
-        fontFamily: 'Nunito',
-        fontSize: 16, // Adjust the font size as needed
-        color: '#fff',
-        width: '94%', // Use percentage values differently than in web, adjust accordingly
-        minHeight: 400,
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        padding: 16, // Adjust padding as needed
-        backgroundColor: 'rgba(0,0,0,0.4)',
-      },
-      startSectionHeading: {
-        backgroundColor: 'lightpink',
-        color: 'firebrick',
-        fontWeight: 'bold',
-        padding: 8, // Adjust padding as needed
-        marginBottom: 8, // Adjust margin as needed
-      },
-      offscreen: {
-        position: 'absolute',
-        left: -9999,
-      },
-      startForm: {
-        // Styles for startForm
-      },
-      startForm: {
-        fontFamily: 'Nunito',
-        fontSize: 22,
-        color: '#fff',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
-        flexGrow: 1,
-        paddingBottom: 16, // Adjust as needed
-      },
-      startLabel: {
-        marginTop: 16,
-      },
-      inputUsername: {
-        fontFamily: 'Nunito',
-        fontSize: 22,
-        padding: 4, // Adjust padding as needed
-        borderRadius: 8, // Adjust border radius as needed
-      },
-      inputPassword: {
-        fontFamily: 'Nunito',
-        fontSize: 22,
-        padding: 4, // Adjust padding as needed
-        borderRadius: 8, // Adjust border radius as needed
-      },
-      startButton: {
-        fontFamily: 'Nunito',
-        fontSize: 22,
-        padding: 8, // Adjust padding as needed
-        borderRadius: 8, // Adjust border radius as needed
-        backgroundColor: '#cf7d03',
-        borderColor: '#cf7d03',
-        color: '#fff',
-      },
-      forgotPassword: {
-        fontSize: 14, // Adjust font size as needed
-        width: '100%',
-      },
-      startText: {
-        textAlign: 'center',
-        fontSize: 14, // Adjust font size as needed
-        marginBottom: 15,
-      },
-      startCreate: {
-        fontFamily: 'Nunito',
-        fontSize: 22,
-        padding: 4, // Adjust padding as needed
-        backgroundColor: 'black',
-        color: '#fff',
-      },
-  });
+  selectedValuesContainer: {
+    marginTop: 20,
+    paddingHorizontal: 10,
+    alignItems: "center",
+  },
+  selectedValuesText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+    width: "80%",
+    height: "40px",
+    backgroundColor: "#2980b9",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "5px",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  Input: {
+    width: "80%",
+    height: "40px",
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    marginBottom: "20px",
+    padding: "10px",
+    borderRadius: "5px",
+  },
+  startBody: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  exclusiveText: {
+    height: "20%",
+    width: "15%",
+    marginRight: "15%",
+    fontWeight: "1000",
+    fontSize: 4,
+    color: "white",
+  },
+  startContainer: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  startSection: {
+    position: "relative",
+    fontFamily: "Nunito",
+    fontSize: 16, // Adjust the font size as needed
+    color: "#fff",
+    width: "94%", // Use percentage values differently than in web, adjust accordingly
+    minHeight: 400,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    padding: 16, // Adjust padding as needed
+    backgroundColor: "rgba(0,0,0,0.4)",
+  },
+  startSectionHeading: {
+    backgroundColor: "lightpink",
+    color: "firebrick",
+    fontWeight: "bold",
+    padding: 8, // Adjust padding as needed
+    marginBottom: 8, // Adjust margin as needed
+  },
+  offscreen: {
+    position: "absolute",
+    left: -9999,
+  },
+  startForm: {
+    // Styles for startForm
+  },
+  startForm: {
+    fontFamily: "Nunito",
+    fontSize: 22,
+    color: "#fff",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    flexGrow: 1,
+    paddingBottom: 16, // Adjust as needed
+  },
+  startLabel: {
+    marginTop: 16,
+  },
+  inputUsername: {
+    fontFamily: "Nunito",
+    fontSize: 22,
+    padding: 4, // Adjust padding as needed
+    borderRadius: 8, // Adjust border radius as needed
+  },
+  inputPassword: {
+    fontFamily: "Nunito",
+    fontSize: 22,
+    padding: 4, // Adjust padding as needed
+    borderRadius: 8, // Adjust border radius as needed
+  },
+  startButton: {
+    fontFamily: "Nunito",
+    fontSize: 22,
+    padding: 8, // Adjust padding as needed
+    borderRadius: 8, // Adjust border radius as needed
+    backgroundColor: "#cf7d03",
+    borderColor: "#cf7d03",
+    color: "#fff",
+  },
+  forgotPassword: {
+    fontSize: 14, // Adjust font size as needed
+    width: "100%",
+  },
+  startText: {
+    textAlign: "center",
+    fontSize: 14, // Adjust font size as needed
+    marginBottom: 15,
+  },
+  startCreate: {
+    fontFamily: "Nunito",
+    fontSize: 22,
+    padding: 4, // Adjust padding as needed
+    backgroundColor: "black",
+    color: "#fff",
+  },
+  questionBox: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    width: "80%",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  questionTextContainer: {
+    backgroundColor: "#ECECEC",
+    padding: 10,
+    borderRadius: 5,
+  },
+  questionText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  checkboxOption: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  checkboxItem: {
+    alignItems: "center",
+  },
+  checkbox: {
+    // Add any necessary styling here
+  },
+  dropdownContainer: {
+    marginTop: 20, // Adjust this value to move the dropdown down if needed
+  },
+  exclusiveText: {
+    // Your existing styles for the "Next" button
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 400,
+    height: 220,
+    bottom: 0,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+});
 
   export default styles;
--- screens/Page2.js
+++ screens/Page2.js
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+"react";
 import {
   View,
   Text,
-  TextInput,
   TouchableOpacity,
   StatusBar,
   Button,
+  ImageBackground,
+  Image,
 } from "react-native";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { CheckBox } from "react-native-elements";
 import { Picker } from "@react-native-picker/picker";
 import styles from "../components/Styles";
+import { StyleSheet } from "react-native";
 import Footer from "../Footer";
+import Sky from "../Images/sky.png";
+import { image } from "react-native";
+import { useState } from "react";
 
 export default function Page2({ navigation }) {
   const [choice, setChoice] = useState(false);
@@ -20,49 +26,82 @@ export default function Page2({ navigation }) {
 
   return (
     <View style={styles.startBody}>
-      <View style={styles.questionBox}>
-        <Text style={styles.questionText}>Are you traveling with family?</Text>
-        <View style={styles.checkboxOption}>
-          <Text
-            onPress={() => {
-              setChoice(true);
-            }}
-          >
-            Yes
-          </Text>
-          <CheckBox checked={choice} onPress={() => setChoice(true)} />
-
-          <Text
-            style={styles.checkbox} // Add any necessary styling here
-            onPress={() => {
-              setChoice(false);
-            }}
-          >
-            No
-          </Text>
-          <CheckBox checked={!choice} onPress={() => setChoice(false)} />
-        </View>
-        {choice ? (
-          <View style={styles.dropdownContainer}>
-            <Text>Select Account Holder:</Text>
-            <Picker
-              selectedValue={selectedName}
-              onValueChange={(itemValue) => setSelectedName(itemValue)}
-            >
-              {names.map((name, index) => (
-                <Picker.Item key={index} label={name} value={name} />
-              ))}
-            </Picker>
+      <ImageBackground
+        source={require("../Images/sky.png")}
+        style={background.backgroundImage}
+      >
+        <View style={styles.questionBox}>
+          <View style={styles.questionTextContainer}>
+            <Text style={styles.questionText}>
+              Are you traveling with family?
+            </Text>
           </View>
-        ) : null}
-      </View>
-      
-      <View style={styles.exclusiveText}>
-        <TouchableOpacity onPress={() => navigation.navigate("Page3")}>
-          <Text>Page 3</Text>
-        </TouchableOpacity>
-      </View>
+          <View style={styles.checkboxOption}>
+            <View style={styles.checkboxItem}>
+              <Text
+                onPress={() => {
+                  setChoice(true);
+                }}
+              >
+                Yes
+              </Text>
+              <CheckBox checked={choice} onPress={() => setChoice(true)} />
+            </View>
+            <View style={styles.checkboxItem}>
+              <Text
+                onPress={() => {
+                  setChoice(false);
+                }}
+              >
+                No
+              </Text>
+              <CheckBox checked={!choice} onPress={() => setChoice(false)} />
+            </View>
+          </View>
+          {choice ? (
+            <View style={styles.dropdownContainer}>
+              <Text>Select Account Holder:</Text>
+              <Picker
+                selectedValue={selectedName}
+                onValueChange={(itemValue) => setSelectedName(itemValue)}
+              >
+                {names.map((name, index) => (
+                  <Picker.Item key={index} label={name} value={name} />
+                ))}
+              </Picker>
+            </View>
+          ) : null}
+        </View>
+        <View style={styles.exclusiveText}>
+          <TouchableOpacity onPress={() => navigation.navigate("Page3")}>
+            <Text>Page 3</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+
       <Footer />
     </View>
   );
 }
+
+const background = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+});
+
+const style = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 400,
+    height: 220,
+    bottom: 0,
+  },
+});
